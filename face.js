422d4e40653a9a643c09e2a3f623930c1cbb1a50
bryantomoregie <unclebryant@gmail.com>
2020-09-10 20:56:59 -0500

    face api done

--- face.js
+++ face.js
@@ -1,58 +1,88 @@
-//document.body.style.backgroundImage = "url('https://upload.wikimedia.org/wikipedia/commons/c/c3/RH_Louise_Lillian_Gish.jpg')"
+let analyzeButton = document.getElementById("analyzeButton");
+let responseTextArea = document.getElementById("responseTextArea");
+let sourceImage = document.getElementById("sourceImage");
+
 
 function processImage() {
-  // Replace <Subscription Key> with your valid subscription key.
-  var subscriptionKey = "11aee8f055024ae8995d500c85eeabd8";
+    // Replace <Subscription Key> with your valid subscription key.
+    var subscriptionKey = "11aee8f055024ae8995d500c85eeabd8";
 
-  var uriBase =
-    "https://bryantomoregie.cognitiveservices.azure.com/face/v1.0/detect";
+    var uriBase = "https://bryantomoregie.cognitiveservices.azure.com/face/v1.0/detect";
+    let url = new URL(uriBase)
 
-  // Request parameters.
-  var params = {
-    returnFaceId: "true",
-    returnFaceLandmarks: "false",
-    returnFaceAttributes: "age,gender",
+    //Get image URL
+    imgURL = document.getElementById("inputImage").value;
+
+    // Display the image.
+    sourceImage.src = imgURL;
+
+    // Request parameters.
+    var params = {
+      returnFaceId: "true",
+      returnFaceLandmarks: "false",
+      returnFaceAttributes:
+        "age,gender"
+    };
+
+    
+    Object.keys(params).forEach((key) => 
+    url.searchParams.append(key, params[key]))
+    
+   var myHeader =  new Headers({
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': subscriptionKey
+});
+
+   var initObject = {
+    method: 'POST',
+    body: '{"url": ' + '"' + imgURL + '"}',
+    headers: myHeader,
+    mode: 'cors'
   };
-
-  // Display the image.
-  var sourceImageUrl = document.getElementById("inputImage").value;
-  document.querySelector("#sourceImage").src = sourceImageUrl;
-
-  // Perform the REST API call.
-  $.ajax({
-    url: uriBase + "?" + $.param(params),
-
-    // Request headers.
-    beforeSend: function (xhrObj) {
-      xhrObj.setRequestHeader("Content-Type", "application/json");
-      xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
-    },
-
-    type: "POST",
-
-    // Request body.
-    data: '{"url": ' + '"' + sourceImageUrl + '"}',
-  })
-
-    .done(function (data) {
-      // Show formatted JSON on webpage.
-    // document.getElementById("output").innerHTML = data[0].faceAttributes.gender
-      console.log(data[0].faceAttributes.gender)
-      $("#responseTextArea").val(JSON.stringify(data[0].faceAttributes.gender));
-    })
-
-    .fail(function (jqXHR, textStatus, errorThrown) {
-      // Display error message.
-      var errorString =
-        errorThrown === ""
-          ? "Error. "
-          : errorThrown + " (" + jqXHR.status + "): ";
-      errorString +=
-        jqXHR.responseText === ""
-          ? ""
-          : jQuery.parseJSON(jqXHR.responseText).message
-          ? jQuery.parseJSON(jqXHR.responseText).message
-          : jQuery.parseJSON(jqXHR.responseText).error.message;
-      alert(errorString);
-    });
+    // var initObject = {
+    //     method: 'POST',
+    //     mode: 'cors',
+    //     headers: new Headers({
+    //         'Content-Type': 'application/json',
+    //         "Ocp-Apim-Subscription-Key": subscriptionKey
+    //     }),
+    //     body: JSON.stringify({
+    //         'url': imageUrl
+    //     })
+    // }
+    var request = new Request(url, initObject);
+    fetch(request).then((response)=>{
+        if (response.ok) {
+          return response.json();
+        } else {
+          return Promise.reject(new Error(response.statusText));
+        }
+      }).then((response)=>{
+        for(let x in response[0].faceAttributes){
+            responseTextArea.append(`${x}: ${response[0].faceAttributes[x]}`)
+            responseTextArea.append(document.createElement("br"))
+       
+        }
+        // document.getElementById("wrapper").style.display = "block";
+        // if(!response[0]) {
+        //   document.getElementById("noattributes").style.display = "block";
+        //   document.getElementById("attributes").style.display = "none";
+        // } else {
+        // document.getElementById("noattributes").style.display = "none";
+        //   document.getElementById("attributes").style.display = "block"
+        // age.innerHTML = response[0].faceAttributes.age;
+        // gender.innerHTML = response[0].faceAttributes.gender; 
+        // }
+    
+      })
+    // var request = new Request(url, initObject);
+    // fetch(request).then((value) => {
+    //     return value.json()
+    // }).then((value) => {console.log(value)}).catch((err) => {
+    //     console.log(err)
+    // })
 }
+
+analyzeButton.addEventListener('click', processImage)
+
+
